f6c13d5cc959ea178e0aab7c5fc190208861297a
David Broll <david.broll@student.hs-rm.de>
2017-11-29 12:52:33 +0100

    added Media Ref

--- src/app/secure/track-details/track-details.component.ts
+++ src/app/secure/track-details/track-details.component.ts
@@ -24,6 +24,8 @@ export class TrackDetailsComponent implements OnInit {
     RATING_COMMENT_LENGTH = 100;
     LETTERS = {1:"A", 2:"B", 3:"C", 4:"D", 5:"E", 6:"F", 7:"G", 8:"H", 9:"I", 10:"J", 11:"K", 12:"L", 13:"M", 14:"N", 15:"O", 16:"P", 17:"Q", 18:"R", 19:"S", 20:"T", 21:"U", 22:"V", 23:"W", 24:"X", 25:"Y", 26:"Z"};
 
+    MEDIA_REF= "https://l2vba9toe9.execute-api.us-west-2.amazonaws.com/api/media/"
+
     track: Track;
     sponsors: Sponsor[];
     organisations: Organisation[];
